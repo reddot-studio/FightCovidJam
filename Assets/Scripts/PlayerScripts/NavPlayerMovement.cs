ccd42498c903ab78ca558ebae41b86a4aaead445
Jordi Pardo <jordipardogutierrez@gmail.com>
2020-04-16 21:38:01 +0200

    Animations

--- Assets/Scripts/PlayerScripts/NavPlayerMovement.cs
+++ Assets/Scripts/PlayerScripts/NavPlayerMovement.cs
@@ -2,31 +2,70 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.EventSystems;
 
 public class NavPlayerMovement : MonoBehaviour
 {
 
     public NavMeshAgent agent;
+    Animator anim;
+
+    Camera cam;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        cam = Camera.main;
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(1))
+
+
+        if (Input.GetMouseButtonDown(0))
         {
 
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
             RaycastHit _hit;
 
             if (Physics.Raycast(ray, out _hit))
             {
                 agent.SetDestination(_hit.point);
             }
+        }
+
+        //if (Input.touchCount > 0)
+        //{
+        //    //If touching UI ignore touch ( UI still works )
+        //    if (EventSystem.current.IsPointerOverGameObject())
+        //        return;
+
+        //    Touch touch = Input.GetTouch(0);
+        //    if (touch.phase == TouchPhase.Began)
+        //    {
+        //        Ray ray = cam.ScreenPointToRay(touch.position);
+        //        RaycastHit _hit;
+
+        //        if (Physics.Raycast(ray, out _hit))
+        //        {
+        //            agent.SetDestination(_hit.point);
+        //        }
+        //    }
+
+        //}
+
+
+
+
+        if (agent.velocity.magnitude > 0.3f)
+        {
+            anim.SetBool("isWalking", true);
+        }
+        if(agent.remainingDistance < 0.5f)
+        {
+            anim.SetBool("isWalking", false);
         }
     }
 }

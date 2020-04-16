479ee6690670a007539f4dac33c502e65a545e02
Jordi Pardo <jordipardogutierrez@gmail.com>
2020-04-16 19:27:01 +0200

    Mayk commit

--- Assets/Scripts/Tools/CameraSmoothFollow.cs
+++ Assets/Scripts/Tools/CameraSmoothFollow.cs
@@ -9,11 +9,12 @@ public class CameraSmoothFollow : MonoBehaviour
 
     public float smoothSpeed = 0.125f;
     public Vector3 offset;
+    private Vector3 velocity = Vector3.zero;
 
-    void FixedUpdate()
+    void Update()
     {
         Vector3 desiredPosition = target.position + offset;
-        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        Vector3 smoothedPosition = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, smoothSpeed);
         transform.position = smoothedPosition;
 
         transform.LookAt(target);

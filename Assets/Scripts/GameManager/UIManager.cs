620cb0081b8a3b7afe542b3b58279ad6190959a1
MayKoder <sg.miquel@gmail.com>
2020-04-18 13:53:23 +0200

    End game ui

---
+++ Assets/Scripts/GameManager/UIManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+
+    public GameObject endCanvas;
+    TextMeshProUGUI scoreText;
+
+
+
+
+    public void LoadEndScene()
+    {
+        endCanvas.SetActive(true);
+        GameManager.instance.gameObject.SetActive(false);
+    }
+
+    public void CloseGame()
+    {
+        Application.Quit();
+    }
+
+    public void RestartGame()
+    {
+        Debug.Log("Restaring game...");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        endCanvas.SetActive(false);
+        GameManager.onLose += LoadEndScene;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
